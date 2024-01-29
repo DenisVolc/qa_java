b80f93a964ac6725df134e3f35199880c58a596a
Denis Volchenko <volchenko.di@infotek.ru>
2024-01-29 11:19:23 +0300

    Удалил лишний класс. Поправил pom что бы не было ошибки кодировки

--- src/main/java/com/example/Service.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Service {
-    public static void main(String[] args) throws Exception {
-        Feline feline = new Feline();
-        Lion Boniface = new Lion("Самец",feline);
-    }
-}
-
